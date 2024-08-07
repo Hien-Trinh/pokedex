39ea9021419c8e48e270d373cdc9a3446ad31190
Hien-Trinh <hientrinhminh32@gmail.com>
2024-08-07 10:19:19 +0700

    added explore specific area (list pokemon in an area)

---
+++ command_explore.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandExplore(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("expected 1 argument, got %d", len(args))
+	}
+
+	name := args[0]
+	resp, err := cfg.pokeapiClient.GetLocation(name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Exploring %s...", name)
+	fmt.Println()
+	fmt.Println("Found the following Pokemon:")
+	for _, pokemon_encounters := range resp.PokemonEncounters {
+		fmt.Printf("- %s\n", pokemon_encounters.Pokemon.Name)
+	}
+
+	return nil
+}
